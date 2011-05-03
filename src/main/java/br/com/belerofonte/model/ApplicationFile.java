ae1ecebeacd5fd5682d7a756941b513b843af2f7
Marcelo Tozzi <marcelotozzidelima@gmail.com>
2011-05-02 22:32:15 -0300

    adicionado atributos ao ApplicationFile

--- src/main/java/br/com/belerofonte/model/ApplicationFile.java
+++ src/main/java/br/com/belerofonte/model/ApplicationFile.java
@@ -1,5 +1,106 @@
 package br.com.belerofonte.model;
 
-public class ApplicationFile {
+import java.util.Calendar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.NotEmpty;
+
+@Entity
+public class ApplicationFile {
+	@Id @GeneratedValue private Long id;
+	@NotNull @NotEmpty private String name;
+	@NotNull @NotEmpty private String nameOfFile;
+	private String description;
+	@NotNull @NotEmpty private Long sizeOfFile;
+	@NotNull @NotEmpty private String contentType;
+	@NotNull @NotEmpty private Plataform plataform;
+	@NotNull @NotEmpty private ApplicationType applicationType;
+	@Temporal(TemporalType.DATE) private Calendar uploadDate;
+	private Long numberOfDownloads;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNameOfFile() {
+		return nameOfFile;
+	}
+
+	public void setNameOfFile(String nameOfFile) {
+		this.nameOfFile = nameOfFile;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getSizeOfFile() {
+		return sizeOfFile;
+	}
+
+	public void setSizeOfFile(Long sizeOfFile) {
+		this.sizeOfFile = sizeOfFile;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public Plataform getPlataform() {
+		return plataform;
+	}
+
+	public void setPlataform(Plataform plataform) {
+		this.plataform = plataform;
+	}
+
+	public ApplicationType getApplicationType() {
+		return applicationType;
+	}
+
+	public void setApplicationType(ApplicationType applicationType) {
+		this.applicationType = applicationType;
+	}
+
+	public Calendar getUploadDate() {
+		return uploadDate;
+	}
+
+	public void setUploadDate(Calendar uploadDate) {
+		this.uploadDate = uploadDate;
+	}
+
+	public Long getNumberOfDownloads() {
+		return numberOfDownloads;
+	}
+
+	public void setNumberOfDownloads(Long numberOfDownloads) {
+		this.numberOfDownloads = numberOfDownloads;
+	}
 }
--- src/main/java/br/com/belerofonte/service/ApplicationFileService.java
+++ src/main/java/br/com/belerofonte/service/ApplicationFileService.java
@@ -3,7 +3,11 @@ package br.com.belerofonte.service;
 import br.com.belerofonte.dao.ApplicationFileDAO;
 import br.com.belerofonte.model.ApplicationFile;
 import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.RequestScoped;
 
+@Component
+@RequestScoped
 public class ApplicationFileService {
 
 	private ApplicationFileDAO applicationFileDAO;
